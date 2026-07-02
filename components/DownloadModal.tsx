aff2f7cd3e7ae25e4fbc1627962bffddf1f4e620
deogratias228 <gwoblesse@gmail.com>
2026-07-02 15:35:50 +0000

    rgpd

--- components/DownloadModal.tsx
+++ components/DownloadModal.tsx
@@ -58,8 +58,8 @@ export default function DownloadModal({ isOpen, onClose, downloadUrl }: Download
       }
 
       setStatus("success");
-      @ts-ignore
-      fbq("track", "Lead", { name, whatsapp }); // <-- important
+      //@ts-ignore
+      fbq("track", "Download"); // <-- important
       triggerFileDownload();
     } catch {
       setError("Connexion impossible. Vérifiez votre internet et réessayez.");
