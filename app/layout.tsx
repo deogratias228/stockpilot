ca50f25c5bc93b0ea31dbc631e65f22076acaade
deogratias228 <gwoblesse@gmail.com>
2026-07-02 15:20:30 +0000

    add Meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script"; // <-- important
 import "./globals.css";
 
 const inter = Inter({
@@ -21,7 +22,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className="scroll-smooth">
-      <body className={`${inter.variable} font-sans antialiased bg-white text-slate-900 overflow-x-hidden`}>
+      <head>
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '963255906755869'); 
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            src="https://www.facebook.com/tr?id=963255906755869&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
+      <body
+        className={`${inter.variable} font-sans antialiased bg-white text-slate-900 overflow-x-hidden`}
+      >
         {children}
       </body>
     </html>
